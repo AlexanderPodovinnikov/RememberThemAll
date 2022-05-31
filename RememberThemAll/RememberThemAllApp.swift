410ff0fff097527d45fc9be86b48868df865c4da
AlexanderPodovinnikov <art.iq@hotmail.com>
2022-05-31 13:09:43 +0300

    Initial Commit

---
+++ RememberThemAll/RememberThemAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberThemAllApp.swift
+//  RememberThemAll
+//
+//  Created by Alex Po on 31.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RememberThemAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
